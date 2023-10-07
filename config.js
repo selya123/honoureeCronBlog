105e81d45ca24bfe7b7921a5d62c0ea44c39bb39
Abdul Momin <66585914+abdulmomin956@users.noreply.github.com>
2023-10-07 22:35:13 +0600

    signup part copy from hone.honouree

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ import { version } from './package.json'
 export const TOKEN = 'HON'
 export const IS_HIVE = true
 export const APP_DOMAIN = 'https://honouree.com'
+// export const APP_DOMAIN = 'http://localhost:8080'
 export const APP_TITLE = 'Honouree'
 export const APP = `honouree/${version}`
 export const IMAGES_CDN = 'https://images.hive.blog/'
